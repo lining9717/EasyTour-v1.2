4a598a81d7f66b1d05b12cf135e0834137c436d8
Hubertyori <446526041@qq.com>
2018-04-08 01:08:58 +0800

    first commit

---
+++ app/src/main/java/com/example/lining/easytour/GuiderSettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.lining.easytour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuiderSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guider_setting);
+    }
+}
