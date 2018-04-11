cbee3d6133f5c2d92e12cb14216c0179ee143b1f
Yori <446526041@qq.com>
2018-04-11 20:09:20 +0800

    I added the refresh interface in the App, and it can work when the data get from the internet

--- app/src/main/java/com/example/lining/easytour/orders/OrderDetailActivity.java
+++ app/src/main/java/com/example/lining/easytour/orders/OrderDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.lining.easytour.orders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.lining.easytour.R;
 
@@ -11,6 +12,10 @@ public class OrderDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+
+    }
+
+    public void btn_order_detail_communicateClick(View view) {
 
     }
 }
